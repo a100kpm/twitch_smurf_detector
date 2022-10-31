c39704b247c8c71654ecac870db875774897fd6b
a100kpm <iannis3toussaint@gmail.com>
2022-10-31 17:01:32 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,39 @@
+from getter import *
+find_function()
+
+
+#note if som1 plays only party with the exact same people; by changing account 
+#he can make the program thinks he is one of his teammate
+#and never detect the smurf
+
+# =============================================================================
+# =============================================================================
+# # # geting started:
+# # # get all template (see preprocess)
+# # # get your twitch credidentials (see variables)
+# # # get your stratz credidentials (see variables)
+# # # setup a database (see variables)
+# # # launch collect_data() (see bellow)
+# # # launch associate_player_id() (see bellow)
+# # # find_function() does not require full name of function, ex find_function('associat')
+# # # will find the function associate_player_id
+# =============================================================================
+# =============================================================================
+
+
+# =============================================================================
+# # get twitch's stream data
+# collect_data()
+# =============================================================================
+
+
+# =============================================================================
+# # process data to identify accounts present on each stream
+# associate_player_id()
+# =============================================================================
+
+
+# =============================================================================
+# # give an other chance to the empty game_id row
+# processed_cleaner()
+# =============================================================================
